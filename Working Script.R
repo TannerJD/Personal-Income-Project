36a62ce8f36f1778b21675fec367549e47de7c1d
Jacob Tanner <jdtanner@mail.roanoke.edu>
2023-05-15 15:29:49 -0400

    cleaned first degree data and income data

--- Working Script.R
+++ Working Script.R
@@ -15,6 +15,8 @@ Sys.setenv(VROOM_CONNECTION_SIZE = 50000072)
 
 
 require(readr)
+require(magrittr)
+require(dplyr)
 
 current_file_path = dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(current_file_path)
@@ -39,9 +41,39 @@ first_degree_data$`Label (Grouping)` = trimws(first_degree_data$`Label (Grouping
 temp_cols = (grep("Total!!Estimate", names(indmix_data), value = TRUE))
 print(temp_cols)
 indmix_mod_data = indmix_data[, c("Label (Grouping)", temp_cols)]
-
 indmix_mod_data = indmix_mod_data[-c(2,9,13,16,20,23),]
+
+
+
+temp_cols = (grep("!!Estimate", names(first_degree_data), value = TRUE))
+print(temp_cols)
+first_degree_mod_data = first_degree_data[, c("Label (Grouping)", temp_cols)]
+first_degree_mod_data[is.na(first_degree_mod_data)] = 0
+
+
+
+temp_cols = (grep("!!Estimate", names(income_data), value = TRUE))
+income_mod_data = income_data[, c("Label (Grouping)", temp_cols)]
+
+temp_cols_2 = (grep("!!Percent", names(income_mod_data), value = TRUE))
+print(temp_cols_2)
+temp_cols_3 = (grep("!!Median", names(income_mod_data), value = TRUE))
+print(temp_cols_3)
+
+income_mod_data[temp_cols_2][is.na(income_mod_data[temp_cols_2])] = "0.0%"
+
+income_mod_data <- income_mod_data %>%
+  mutate(across(all_of(temp_cols_3), ~as.numeric(gsub(",", "", .))), .keep = "unused")
+
+income_mod_data[is.na(income_mod_data)] = 0
+  
+  
+# income_mod_data <- income_mod_data %>% mutate(across(all_of(temp_cols_2), ~ifelse(is.na(.), "0.0%", .)))
+
+  
+  
 # sum(indmix_mod_data$`Autauga County, Alabama!!Total!!Estimate`)/2
+# print(sum(first_degree_data$`Autauga County, Alabama!!Estimate`, na.rm = TRUE)/2)
 
 
 
